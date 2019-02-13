6c63a7b1695d10aaf357a67367274b099fd20c3f
Rafet ILGIN <rafetilgin@gmail.com>
2019-02-14 00:07:58 +0300

    resize issues corrected

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,14 +1,24 @@
 $(window).resize(function() {
-    if ($(window).width() < 576) {
-        $("#pfSkills").removeClass("subtitleL");
-        $("#pfSkills").addClass("subtitleLNoBorder");
-        $("#sclSkills").removeClass("subtitleR");
-        $("#sclSkills").addClass("subtitleRNoBorder");
-     }
-     else {
-        $("#pfSkills").removeClass("subtitleLNoBorder");
-        $("#pfSkills").addClass("subtitleL");
-        $("#sclSkills").removeClass("subtitleRNoBorder");
-        $("#sclSkills").addClass("subtitleR");
-     }
+   /*if ($(window).width() >= 992) {
+      $("#edu").removeClass("educationsm");
+      $("#edu").addClass("education");
+   }
+   
+   if ($(window).width() < 992) {
+      $("#edu").removeClass("education");
+      $("#edu").addClass("educationsm");
+   }*/
+   
+   if ($(window).width() < 576) {
+      $("#pfSkills").removeClass("subtitleL");
+      $("#pfSkills").addClass("subtitleLNoBorder");
+      $("#sclSkills").removeClass("subtitleR");
+      $("#sclSkills").addClass("subtitleRNoBorder");
+   }
+   else {
+      $("#pfSkills").removeClass("subtitleLNoBorder");
+      $("#pfSkills").addClass("subtitleL");
+      $("#sclSkills").removeClass("subtitleRNoBorder");
+      $("#sclSkills").addClass("subtitleR");
+   }
 })
