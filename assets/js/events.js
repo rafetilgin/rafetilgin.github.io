1bdd2385fde27e84dbda048cce082a8782cc1e7c
Rafet ILGIN <rafetilgin@gmail.com>
2022-10-17 20:51:52 +0200

    pht update

--- assets/js/events.js
+++ assets/js/events.js
@@ -14,16 +14,30 @@ $(window).resize(function() {
       $("#pfSkills").addClass("subtitleLNoBorder");
       $("#sclSkills").removeClass("subtitleR");
       $("#sclSkills").addClass("subtitleRNoBorder");
+      $("div.progress").toggleClass("progress-sm");
+      $("p.skillp").toggleClass("skillp-sm");
    }
    else {
       $("#pfSkills").removeClass("subtitleLNoBorder");
       $("#pfSkills").addClass("subtitleL");
       $("#sclSkills").removeClass("subtitleRNoBorder");
       $("#sclSkills").addClass("subtitleR");
+      $("div.progress-sm").toggleClass("progress");
+      $("p.skillp-sm").toggleClass("skillp");
    }   
 })
 
 $(document).ready(function() {
    
+   if ($(window).width() < 576) {
+      
+      $("div.progress").toggleClass("progress-sm")
+      $("p.skillp").toggleClass("skillp-sm");
+   }
+   else {
+      
+      $("div.progress-sm").toggleClass("progress")
+      $("p.skillp-sm").toggleClass("skillp");
+   }
    
 })
